92e347042a5c311b5e4a23d912c0ed79c29ea75e
greig.mcmahon <greig.mcmahon@stratton.com.au>
2025-06-07 12:33:02 +1000

    cors errror

--- server/server.js
+++ server/server.js
@@ -13,10 +13,13 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigin = (process.env.FRONTEND_URL || "http://localhost:5173").replace(/\/$/, "");
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: allowedOrigin,
   credentials: true,
 }));
+
 app.use(express.json());
 
 // Connect to MongoDB
