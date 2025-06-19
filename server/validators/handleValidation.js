4b4349e58df7ef427fb52db582c031158b1b2190
greig.mcmahon <greig.mcmahon@stratton.com.au>
2025-06-19 15:58:25 +1000

    validation test

--- server/validators/handleValidation.js
+++ server/validators/handleValidation.js
@@ -1,11 +1,10 @@
 // validators/handleValidation.js
 import { validationResult } from "express-validator";
 
-export const handleValidationErrors = (req, res) => {
+export const handleValidationErrors = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return true;
+    return res.status(400).json({ errors: errors.array() });
   }
-  return false;
+  next();
 };
